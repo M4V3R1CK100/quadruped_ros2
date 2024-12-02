1c7457d23ca449a43d27cc4f728e514c0268af80
Juan Saeteros <jumasaet@espol.edu.ec>
2024-12-01 22:09:04 -0500

    Cambios para correr el loop user

--- quadruped_master/quadruped_master/movement.py
+++ quadruped_master/quadruped_master/movement.py
@@ -18,8 +18,8 @@ class MyNode(Node):
     def __init__(self):
         super().__init__("movement_node")
         self.get_logger().info("Nodo creado")
-        self.joint_state_pub = self.create_publisher(JointState, 'joint_states', 10)
-        self.motion_sub = self.create_subscription(MotionParams,"motion_params", self.movement_manage ,10)
+        self.joint_states_pub = self.create_publisher(JointState, 'joint_states', 10)
+        self.motion_sub = self.create_subscription(MotionParams, "motion_params", self.movement_manage, 10)
         self.motion_params = MotionParams()
         self.rotation = 0
         self.speed = 0
@@ -31,36 +31,35 @@ class MyNode(Node):
         self.joint_states.name = ['front_right_joint1', 'front_right_joint2', 'front_left_joint1', 'front_left_joint2', 
                             'back_left_joint1', 'back_left_joint2', 'back_right_joint1', 'back_right_joint2']
 
-        self.get_logger().info(f"Publicador de joint_states creado: {self.joint_state_pub}")
-
-    def send_joint_states(self, joint_goals_list,time_delay):
+    def send_joint_states(self, joint_goals_list, time_delay):
         for goals in joint_goals_list:
             self.node_joint_states = goals   
-            print(goals)
-            self.joint_state.position = goals
-            self.joint_state.header.stamp = self.get_clock().now().to_msg()
-            self.joint_state_pub.publish(self.joint_state)
+            self.joint_states.position = goals
+            self.joint_states.header.stamp = self.get_clock().now().to_msg()  # Actualiza el timestamp antes de publicar
+            self.joint_states_pub.publish(self.joint_states)
+            self.get_logger().info(f"Publicando: {goals}")
             time.sleep(time_delay)
 
-
     def movement_manage(self, msg):
-        #self.get_logger().info(str(msg))  # Cambié a str para asegurar compatibilidad en el mensaje de registro
         self.motion_params = msg  # Guarda el mensaje recibido en self.motion_params para usar en el movimiento
         self.speed = self.motion_params.speed
         self.rotation = self.motion_params.rotation
         if self.motion_params.speed != 0:
-            print("a")
             movement(self, self.motion_params.speed)
         if self.motion_params.rotation != 0:
-            print("b")
             plan = dummy_traslation(0, 0, self.motion_params.rotation, self.node_joint_states, self.rotation)
             self.send_joint_states(plan, 0.2)
         if self.motion_params.traslation_z != 0 or self.motion_params.traslation_x != 0:
-            print("c")
             plan = dummy_traslation(self.motion_params.traslation_x, self.motion_params.traslation_z, 0, self.node_joint_states, self.rotation)
             self.send_joint_states(plan, 0.2)
 
-def Home(node):
+    def publish_joint_states(self, joint_positions: list):
+        self.joint_states.position = joint_positions
+        self.joint_states.header.stamp = self.get_clock().now().to_msg()
+        self.joint_states_pub.publish(self.joint_states)
+        
+
+def Home(node: MyNode):
     stand_1=0.3
     stand_2=1.4
     # up
@@ -68,25 +67,25 @@ def Home(node):
     joint_position_state=[-1.0, 2.2, -1.0, 2.2, -1.0, 2.2, -1.0, 2.2]
     node.joint_states.position = joint_position_state
     node.joint_states.header.stamp = node.get_clock().now().to_msg()
-    node.joint_state_pub.publish(node.joint_states)
+    node.joint_states_pub.publish(node.joint_states)
     node.node_joint_states = joint_position_state
 
     time.sleep(1)
     joint_position_state=[stand_1, stand_2,stand_1,stand_2,stand_1,stand_2,stand_1,stand_2] # stand up principal
     node.joint_states.position = joint_position_state
     node.joint_states.header.stamp = node.get_clock().now().to_msg()
-    node.joint_state_pub.publish(node.joint_states)
+    node.joint_states_pub.publish(node.joint_states)
     node.node_joint_states = joint_position_state
 
-def movement(node, speed):
+def movement(node: MyNode, speed):
     global quit
     node.speed = speed
-    plan = []
-    min_speed = 0
-    max_speed = 5
-    delay_min = 0.1  # en s
-    delay_max = 0.5  # en s
-    length = 0.15
+    plan       = []
+    min_speed  = 0
+    max_speed  = 5
+    delay_min  = 0.1  # en s
+    delay_max  = 0.5  # en s
+    length     = 0.15
     if speed < 0:   
         length = -length
 
@@ -137,20 +136,20 @@ def movement(node, speed):
         node.send_joint_states(plan,time_delay)
 
 
-def user_input_loop(node):
+def user_input_loop(node: MyNode):
     global quit
     stand_1 = 0.3
     stand_2 = 1.4
 
-    Home(node)
-
     while quit == 0:
+        Home(node)
         print("\nMenu:")
         print("1. Stand")
         print("2. Up position")
         print("3. Move (forward/backward)")
         print("4. Translate")
         print("5. Exit")
+
         
         try:
             imp = input("Enter your choice: ")
@@ -158,14 +157,14 @@ def user_input_loop(node):
             if number == 1:
                 print("Setting to Stand position")
                 joint_position_state = [stand_1, stand_2, stand_1, stand_2, stand_1, stand_2, stand_1, stand_2]
-                node.node_joint_states = joint_position_state
+                node.publish_joint_states(joint_position_state)
             elif number == 2:
                 print("Setting to Up position")
-                joint_position_state = [-1, 2.2, -1, 2.2, -1, 2.2, -1, 2.2]
-                node.node_joint_states = joint_position_state
+                joint_position_state = [-1.0, 2.2, -1.0, 2.2, -1.0, 2.2, -1.0, 2.2]
+                node.publish_joint_states(joint_position_state)
                 time.sleep(1)
                 joint_position_state = [stand_1, stand_2, stand_1, stand_2, stand_1, stand_2, stand_1, stand_2]
-                node.node_joint_states = joint_position_state
+                node.publish_joint_states(joint_position_state)
             elif number == 3:
                 user = input("Enter Speed (positive for forward, negative for backward): ")
                 velocity = int(user)
@@ -183,13 +182,15 @@ def user_input_loop(node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = MyNode()
+    movement_node = MyNode()
+
+    Home(movement_node)
     
     # Crear un hilo para user_input_loop
-    user_input_loop(node)
+    # user_input_loop(movement_node)
 
     # Ejecutar el nodo ROS
-    rclpy.spin(node)
+    rclpy.spin(movement_node)
 
     rclpy.shutdown()
 

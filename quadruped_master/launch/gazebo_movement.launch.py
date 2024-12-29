f3b261bde125399dbfd4b36215d3c44039b8d278
Juan Saeteros <jumasaet@espol.edu.ec>
2024-12-29 18:34:57 -0500

    Cambios en launch de Gazebo

--- quadruped_master/launch/gazebo_movement.launch.py
+++ quadruped_master/launch/gazebo_movement.launch.py
@@ -2,20 +2,29 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 import launch
 from launch.substitutions import Command, LaunchConfiguration
-import launch_ros
-import os
-from ament_index_python.packages import get_package_share_directory
-from launch_ros.parameter_descriptions import ParameterValue
-from launch.event_handlers import OnProcessExit
+from launch.conditions import IfCondition, UnlessCondition
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+
+
 
 def generate_launch_description():
+
+    joy = LaunchConfiguration('joy')
+    image = LaunchConfiguration('image')
     
+    joy_arg = DeclareLaunchArgument(
+        'joy', default_value='False'
+    )
+
+    image_arg = DeclareLaunchArgument(
+        'image', default_value='False'
+    )   
     movement_node = Node(
         package='quadruped_master',
         executable='movement_node',
         output='screen'
     )
-    communication_gazebo_node = Node(
+    gazebo_communication_node = Node(
         package='quadruped_master',
         executable='communication_gazebo_node',
         output='screen'
@@ -23,26 +32,59 @@ def generate_launch_description():
     interface_node = Node(
         package='quadruped_app',
         executable='interface_node',
-        output='screen'
-    )
-    quadruped_visualizer = Node(
+        output='screen',
+        condition = UnlessCondition(joy)
+    )  
+
+    quadruped_visualizer_node = Node(
         package='quadruped_app',
         executable='quadruped_visualizer_node',
-        output='screen'
+        output='screen',
+        condition = UnlessCondition(joy)
     )
 
-    raw_to_bytes = Node(
+    raw_to_bytes_node = Node(
         package='quadruped_app',
         executable='raw_to_bytes_node',
-        output='screen'
+        output='screen',
+        condition = UnlessCondition(joy),
+        parameters=[{'camera_topic': '/camera_link/image_raw'}],
     )
 
-    # Retorna el LaunchDescription
-    return launch.LaunchDescription([
-        quadruped_visualizer,
-        raw_to_bytes,
+    joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        output='screen',
+        condition = IfCondition(joy)
+    )    
+    
+    joy_to_motion_node = Node(
+        package='quadruped_master',
+        executable='joy_to_motion_node',
+        output='screen',
+        condition = IfCondition(joy)   
+    )
+
+    image_node = Node(
+        package='quadruped_app',
+        executable='image_node',
+        output='screen',
+        condition = IfCondition(image)   
+    )
+
+    return LaunchDescription([
+        joy_arg,
+        image_arg, #  argumentos
+
         movement_node,
-        communication_gazebo_node,
-        interface_node
+        gazebo_communication_node,
+
+        interface_node,
+        quadruped_visualizer_node,
+        raw_to_bytes_node,
+        joy_node,
+        joy_to_motion_node,
+        image_node,
+
 
     ])

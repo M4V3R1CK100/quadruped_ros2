57a3d59870d13a85346cdcb59e21b995b346a0e7
Juan Saeteros <jumasaet@espol.edu.ec>
2024-12-29 22:59:55 -0500

    Communication

--- quadruped_communication/setup.py
+++ quadruped_communication/setup.py
@@ -7,8 +7,10 @@ package_name = 'quadruped_communication'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(),
+    packages=find_packages(include=['quadruped_communication', 'quadruped_communication.*'], exclude=['test']),
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.launch.py')),  
     ],
@@ -18,11 +20,13 @@ setup(
     maintainer_email='eridamen@espol.edu.ec',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
+    extras_require={
+        'test': ['pytest'],  # Dependencias para pruebas
+    },
     entry_points={
         'console_scripts': [
             'dynamixel_communication=quadruped_communication.u2d2_communication_r2.py:main',
-            'vrep_communication= quadruped_master.vrep_communication:main', 
+            'vrep_communication= quadruped_communication.vrep_communication:main', 
 
         ],
     },

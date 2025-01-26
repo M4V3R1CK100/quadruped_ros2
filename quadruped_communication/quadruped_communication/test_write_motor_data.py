096827c38ae8cd2cf9d4a77a87f0fa71ada47669
Juan Saeteros <jumasaet@espol.edu.ec>
2025-01-26 13:04:53 -0500

    Prueba de escritura de datos

---
+++ quadruped_communication/quadruped_communication/test_write_motor_data.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+from rclpy.node import Node
+from rclpy import init, spin
+from archie_master.msg import MotorData
+from sensor_msgs.msg import JointState
+
+# Globales para manejo de archivos
+ruta_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', '..', 'motor_data')
+new_file = True
+number = 0
+
+
+def write_data(name: str, data: str):
+    """
+    Escribe datos en un archivo con nombre único, basado en el número de archivo.
+    """
+    global new_file, number
+    while True:
+        file_path = os.path.join(ruta_file, f"{number}_motor_{name}.txt")
+        if os.path.exists(file_path):
+            if new_file:
+                number += 1
+            else:
+                with open(file_path, "a") as archivo:
+                    archivo.write(data)
+                break
+        else:
+            with open(file_path, "w") as archivo:
+                archivo.write(data)
+            new_file = False
+            break
+
+
+class MotorDataWriter(Node):
+    """
+    Nodo para escribir datos del mensaje `MotorData` en archivos.
+    """
+
+    def __init__(self):
+        super().__init__("write_motor_data_node")
+
+        self.joint_subscription   = self.create_subscription(JointState, '/joint_goals', self.callback, 10)
+        self.publisher_motor_data = self.create_publisher   (MotorData,  '/motor_data' , 10)
+
+        self.get_logger().warn("The test_write_motor_data_node has been started")
+
+        self.create_timer(0.1, self.update_data)
+        self.motor_position_converted = [0,0,0,0,0,0,0,0,0]
+        self.present_pos = [0,0,0,0,0,0,0,0,0]
+        self.motors = MotorData()
+
+    def update_data(self):
+        for i in self.present_pos:
+            self.present_pos[i]+=0.1
+
+        self.motors.goal_position = self.present_pos
+        self.motors.pres_position = [self.motor_position_converted[0], self.motor_position_converted[1], self.motor_position_converted[2], self.motor_position_converted[3], self.motor_position_converted[4], self.motor_position_converted[5], self.motor_position_converted[6], self.motor_position_converted[7], self.motor_position_converted[8]]
+        self.publisher_motor_data.publish(self.motors)
+
+
+    def callback(self, data: JointState):
+        
+        self.motor_position_converted[0] = data.position[0]*180/3.1416 + 180
+        self.motor_position_converted[1] = data.position[1]*180/3.1416
+        self.motor_position_converted[2] = data.position[2]*180/3.1416
+        self.motor_position_converted[3] = data.position[3]*180/3.1416
+        self.motor_position_converted[4] = data.position[4]*180/3.1416
+        self.motor_position_converted[5] = data.position[5]*180/3.1416
+        self.motor_position_converted[6] = data.position[6]*180/3.1416
+        self.motor_position_converted[7] = data.position[7]*180/3.1416
+        self.motor_position_converted[8] = data.position[8]*180/3.1416
+
+
+def main(args=None):
+    """
+    Función principal para inicializar el nodo y mantenerlo en ejecución.
+    """
+    init(args=args)
+    node = MotorDataWriter()
+    try:
+        spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info("Shutting down write_motor_data_node")
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()

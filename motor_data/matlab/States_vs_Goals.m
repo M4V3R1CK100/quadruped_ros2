dbad3d2b16d5ba61432a888c03bc67f41857a1a1
Juan Saeteros <jumasaet@espol.edu.ec>
2025-02-08 11:22:47 -0500

    Para gráficas de las patas del cuadrúpedo

--- motor_data/matlab/States_vs_Goals.m
+++ motor_data/matlab/States_vs_Goals.m
@@ -1,6 +1,6 @@
 %https://la.mathworks.com/help/robotics/ref/importrobot.html
 
-ndoc  = 1;
+ndoc  = 2;
 joint_goals   = table2array(readtable(strcat('motor_data/',num2str(ndoc),'_goal_position.txt')));
 joint_states  = table2array(readtable(strcat('motor_data/',num2str(ndoc),'_pres_position.txt')));
 
@@ -16,8 +16,8 @@ for j = 2:9
     title("Joint " + (j-1));
     legend("Av. error: " + mean(diff_abs(:,j)))
 end
-xlabel("Iteración")
-ylabel("Error (grados)")
+xlabel("Time (ms)")
+ylabel("Error (degrees)")
 for j = 2:9
     
     figure(2);
@@ -27,7 +27,7 @@ for j = 2:9
     plot(joint_goals(:,j),"LineWidth",2)
     legend('Real Position','Goal Position') 
 end
-xlabel("Iteración")
-ylabel("Desplazamiento (grados)")
+xlabel("Time (ms)")
+ylabel("Position (degrees)")
 
 
